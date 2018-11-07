a004410df28c48c498fa8acf5b45a41eed7072d0
dynamicbalaji <balajiashokkumar@yahoo.com>
2018-11-06 23:32:18 -0600

    Adding PropTypes check for type variable

--- src/components/Burger/BurgerIngredient/BurgerIngredient.js
+++ src/components/Burger/BurgerIngredient/BurgerIngredient.js
@@ -1,4 +1,5 @@
 import React, { Component } from 'react';
+import PropTypes from 'prop-types';
 
 import classes from './BurgerIngredient.module.css';
 
@@ -37,5 +38,9 @@ class BurgerIngredient extends Component {
         return ingredient;
     }
 }
+
+BurgerIngredient.propTypes = {
+    type: PropTypes.string.isRequired
+};
 
 export default BurgerIngredient;

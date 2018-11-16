f11c623eaa38e439e7db6a39bf0ba8e8fc3e0be8
dynamicbalaji <balajiashokkumar@yahoo.com>
2018-11-16 01:20:26 -0600

    Incorporating buttons component in ordersummary

--- src/components/Burger/OrderSummary/OrderSummary.js
+++ src/components/Burger/OrderSummary/OrderSummary.js
@@ -1,5 +1,7 @@
 import React from 'react';
 
+import Button from '../../UI/Button/Button';
+
 const OrderSummary = (props) => {
     const ingSummary = Object.keys(props.ingredients)
         .map(igKey => {
@@ -15,6 +17,8 @@ const OrderSummary = (props) => {
             <p>A delicious burger with following ingredients:</p>        
             <ul>{ingSummary}</ul>
             <p>Continue to Checkout?</p>
+            <Button btnType="Danger" clicked={props.cancel}>CANCEL</Button>
+            <Button btnType="Success" clicked={props.continue}>CONTINUE</Button>
         </>
     );
 }

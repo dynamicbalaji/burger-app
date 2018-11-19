47e8f41497248de4438ffb234e512727674965b0
dynamicbalaji <balajiashokkumar@yahoo.com>
2018-11-19 00:41:19 -0600

    Fixing typo in SideDrawer

--- src/components/Navigation/SideDrawer/SideDrawer.js
+++ src/components/Navigation/SideDrawer/SideDrawer.js
@@ -6,7 +6,7 @@ import Backdrop from './../../UI/Backdrop/Backdrop';
 import classes from './SideDrawer.module.css';
 
 const SideDrawer = (props) => {
-    let attachedClasses = [classes.SideDrawer,  classes.Closed];
+    let attachedClasses = [classes.SideDrawer,  classes.Close];
     if(props.show) {
         attachedClasses = [classes.SideDrawer, classes.Open];
     }

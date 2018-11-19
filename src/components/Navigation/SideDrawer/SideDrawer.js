996673100d5e92d91c09d043f140fe01ab59c2fc
dynamicbalaji <balajiashokkumar@yahoo.com>
2018-11-19 00:24:21 -0600

    Adding changes in sidedrawer to show/hide

--- src/components/Navigation/SideDrawer/SideDrawer.js
+++ src/components/Navigation/SideDrawer/SideDrawer.js
@@ -6,10 +6,15 @@ import Backdrop from './../../UI/Backdrop/Backdrop';
 import classes from './SideDrawer.module.css';
 
 const SideDrawer = (props) => {
+    let attachedClasses = [classes.SideDrawer,  classes.Closed];
+    if(props.show) {
+        attachedClasses = [classes.SideDrawer, classes.Open];
+    }
+
     return (
         <>
             <Backdrop show={props.show} closeModal={props.clicked}/>
-            <div className={classes.SideDrawer}>
+            <div className={attachedClasses.join(' ')}>
                 <Logo height="11%" className={classes.Logo} />
                 <nav>
                     <NavigationItems />

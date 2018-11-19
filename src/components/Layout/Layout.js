900b1da1921c595f4dac220f97d2234c4d6fb9e1
dynamicbalaji <balajiashokkumar@yahoo.com>
2018-11-18 21:04:09 -0600

    Incorporating sidebar in layout component

--- src/components/Layout/Layout.js
+++ src/components/Layout/Layout.js
@@ -1,15 +1,17 @@
 import React from 'react';
 
 import Toolbar from '../Navigation/Toolbar/Toolbar';
+import SideDrawer from './../Navigation/SideDrawer/SideDrawer';
 import classes from './Layout.module.css';
 
 const Layout = (props) => {
     return (
         <>
-        <Toolbar />
-        <main className={classes.Content}>
-        {props.children}
-        </main>
+            <Toolbar />
+            <SideDrawer />
+            <main className={classes.Content}>
+                {props.children}
+            </main>
         </>
     );
 }

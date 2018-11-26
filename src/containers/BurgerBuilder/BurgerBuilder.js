8e7ce458041b6e671beb9df765e41495705c0fcc
dynamicbalaji <balajiashokkumar@yahoo.com>
2018-11-26 16:23:07 -0600

    Fetching ingredients from firebase & using it in the application
    componentDidMount for service calls

--- src/containers/BurgerBuilder/BurgerBuilder.js
+++ src/containers/BurgerBuilder/BurgerBuilder.js
@@ -19,17 +19,21 @@ class BurgerBuilder extends Component {
     constructor(props) {
         super(props);
         this.state = {
-            ingredients: {
-                salad: 0,
-                bacon: 0,
-                cheese: 0,
-                meat: 0
-            },
+            ingredients: null,
             totalPrice: 3,
             purchasable: false,
             showModal: false,
             loading: false
         };
+    }
+
+    componentDidMount() {
+        this.setState({ loading: true });
+        axios.get('/ingredients.json')
+            .then(response => {
+                this.setState({ ingredients: response.data, loading: false });
+            })
+            .catch(error => console.log(error));
     }
 
     updatePurchaseState(ing) {
@@ -87,7 +91,7 @@ class BurgerBuilder extends Component {
     }
 
     continuePurchaseHandler = () => {
-        this.setState({loading: true});
+        this.setState({ loading: true });
         const order = {
             ingredients: this.state.ingredients,
             price: this.state.totalPrice,
@@ -104,10 +108,10 @@ class BurgerBuilder extends Component {
         };
         axios.post('/orders.json', order)
             .then(response => {
-                this.setState({loading: false, showModal: false});
+                this.setState({ loading: false, showModal: false });
             })
             .catch(error => {
-                this.setState({loading: false, showModal: false});
+                this.setState({ loading: false, showModal: false });
             });
     }
 
@@ -118,25 +122,30 @@ class BurgerBuilder extends Component {
         for (let key in disabledInfo) {
             disabledInfo[key] = disabledInfo[key] <= 0; // [salad: true, bacon: false, ...]
         }
-        let orderSummary = <OrderSummary ingredients={this.state.ingredients} 
-                cancel={this.cancelPurchaseHandler}
-                continue={this.continuePurchaseHandler}
-                total={this.state.totalPrice}/>;
-        if(this.state.loading) {
-            orderSummary = <Spinner />;
-        }
-        return (
-            <>
-                <Modal show={this.state.showModal} closeModal={this.cancelPurchaseHandler}>
-                    {orderSummary}
-                </Modal>
-                <Burger ingredients={this.state.ingredients} />
+        let orderSummary = null, burger = <Spinner />;
+        if (this.state.ingredients) {
+            burger = <><Burger ingredients={this.state.ingredients} />
                 <BuildControls addIngredient={this.addIngredientHandler}
                     removeIngredient={(type) => this.removeIngredientHandler(type)}
                     disabledInfo={disabledInfo}
                     price={this.state.totalPrice}
                     purchasable={this.state.purchasable}
-                    showModal={this.showModalHandler} />
+                    showModal={this.showModalHandler} /></>;
+            orderSummary = <OrderSummary ingredients={this.state.ingredients}
+                cancel={this.cancelPurchaseHandler}
+                continue={this.continuePurchaseHandler}
+                total={this.state.totalPrice} />;
+        }
+        if (this.state.loading) {
+            orderSummary = <Spinner />;
+        }
+
+        return (
+            <>
+                <Modal show={this.state.showModal} closeModal={this.cancelPurchaseHandler}>
+                    {orderSummary}
+                </Modal>
+                {burger}
             </>
         );
     }

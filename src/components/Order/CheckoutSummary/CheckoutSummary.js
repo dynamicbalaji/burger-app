17b703dad66e6521069d6b89dc8676422088ef04
dynamicbalaji <balajiashokkumar@yahoo.com>
2018-12-06 17:01:08 -0600

    Adding initial version of checkoutsummary component

---
+++ src/components/Order/CheckoutSummary/CheckoutSummary.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+import classes from './CheckoutSummary.module.css';
+
+const CheckoutSummary = (props) => {
+    return (
+        <div>
+            
+        </div>
+    );
+}
+
+export default CheckoutSummary;

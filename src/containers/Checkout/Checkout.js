87a19846807227810c3316168afaac91b8e72080
dynamicbalaji <balajiashokkumar@yahoo.com>
2018-12-10 00:33:11 -0600

    *Adding inline routing for ContactData in Checkout container
    <Route path={this.props.match.path + '/contact-data'}
                        component={ContactData}/>

--- src/containers/Checkout/Checkout.js
+++ src/containers/Checkout/Checkout.js
@@ -1,6 +1,8 @@
 import React, { Component } from 'react';
+import { Route } from 'react-router-dom';
 
 import CheckoutSummary from './../../components/Order/CheckoutSummary/CheckoutSummary';
+import ContactData from './ContactData/ContactData';
 
 class Checkout extends Component {
 
@@ -32,6 +34,8 @@ class Checkout extends Component {
                 <CheckoutSummary ingredients={this.state.ingredients}
                     onCheckoutCancelled={this.checkoutCancelledHandler}
                     onCheckoutContinued={this.checkoutContinuedHandler}/>
+                <Route path={this.props.match.path + '/contact-data'}
+                    component={ContactData}/>
             </div>
         );
     }

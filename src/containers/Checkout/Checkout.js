1865c0049ce6661cb2430d34a53a975e6f1e3fdc
dynamicbalaji <balajiashokkumar@yahoo.com>
2018-12-10 00:14:00 -0600

    *Retrieving & setting params to state object in checkout container
    const query = new URLSearchParams(this.props.location.search);
            const ingredients = {};
            for(let param of query.entries()){
                // [salad, 0]
                ingredients[param[0]] = +param[1];
            }

--- src/containers/Checkout/Checkout.js
+++ src/containers/Checkout/Checkout.js
@@ -5,12 +5,17 @@ import CheckoutSummary from './../../components/Order/CheckoutSummary/CheckoutSu
 class Checkout extends Component {
 
     state = {
-        ingredients: {
-            salad: 1,
-            bacon: 1,
-            cheese: 2,
-            meat: 3
+        ingredients: {}
+    }
+
+    componentDidMount() {
+        const query = new URLSearchParams(this.props.location.search);
+        const ingredients = {};
+        for(let param of query.entries()){
+            // [salad, 0]
+            ingredients[param[0]] = +param[1];
         }
+        this.setState({ingredients});
     }
 
     checkoutCancelledHandler = () =>{
